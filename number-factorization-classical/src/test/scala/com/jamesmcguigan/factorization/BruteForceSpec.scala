3cb08f20366831e15a1eca56a8dc1fcaae832e3c
James McGuigan <james.mcguigan.github@gmail.com>
2018-12-11 08:40:49 +0000

    number-factorization-classical | convert BruteForceSpec to FreeSpec

--- number-factorization-classical/src/test/scala/com/jamesmcguigan/factorization/BruteForceSpec.scala
+++ number-factorization-classical/src/test/scala/com/jamesmcguigan/factorization/BruteForceSpec.scala
@@ -1,6 +1,6 @@
 package com.jamesmcguigan.factorization
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{FreeSpec, Matchers}
 
 
 object BruteForceSpec {
@@ -8,15 +8,20 @@ object BruteForceSpec {
   val knownPrimes : Array[Int] = Array(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541)
 }
 
-class BruteForceSpec extends FlatSpec with Matchers {
+class BruteForceSpec extends FreeSpec with Matchers {
 
-    "isPrime" should s"match first knownPrimes" in {
-      for (n <- 1 to BruteForceSpec.knownPrimes.max) {
-        val isPrime = BruteForce.isPrime(n)
-        if (BruteForceSpec.knownPrimes contains n) {
-          isPrime shouldEqual true
+    "isPrime" - {
+      for (n <- 1 to BruteForceSpec.knownPrimes.max ) {
+        if( BruteForceSpec.knownPrimes contains n ) {
+          s"match knownPrimes: ${n}" in {
+            val isPrime = BruteForce.isPrime(n)
+            isPrime shouldEqual true
+          }
         } else {
-          isPrime shouldEqual false
+          s"not match knownPrimes: ${n}" in {
+            val isPrime = BruteForce.isPrime(n)
+            isPrime shouldEqual false
+          }
         }
       }
     }
